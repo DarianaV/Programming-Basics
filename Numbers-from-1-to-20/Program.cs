05f248f37a994a953b566abccd71a94c23d3d692
Dariana <darianastefanova@mail.bg>
2018-04-17 19:45:45 +0300

    Upload basics tasks

---
+++ Numbers-from-1-to-20/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers_from_1_to_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 1; number <=20; number++)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ TriangleOfStars/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleOfStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char star = '*';
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(star);
+                }
+                Console.WriteLine();
+            }           
+        }
+    }
+}
